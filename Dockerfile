6eddd7036ff3af497810c7fd1df8e088d1ace709
Orie Steele <orie@transmute.industries>
2022-04-22 15:48:00 -0500

    :rocket:

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,7 @@ ADD . /app
 WORKDIR /app
 
 # We are installing a dependency here directly into our app source dir
-RUN pip install --target=/app yamllint
+RUN pip install --target=/app openai
 
 # A distroless container image with Python and some basics like SSL certificates
 # https://github.com/GoogleContainerTools/distroless
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import os
 import sys
+import openai
 
 def main():
-  
-    api_key = os.environ["INPUT_API-KEY"]
+    openai.api_key = os.environ["INPUT_API-KEY"]
     input_text = os.environ["INPUT_TEXT"]
 
-    print(api_key, input_text)
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=input_text[0:4000] + "\n\nTl;dr",
+        temperature=0.7,
+        max_tokens=60,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
 
-    print(f"::set-output name=text::{input_text}")
-
+    output_text = response.choices[0].text
+    print(f"::set-output name=text::{output_text}")
     sys.exit(0)
 
 
