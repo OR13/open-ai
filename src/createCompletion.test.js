8c5af7520710a5c4085e76bf03a7baa421506278
Orie Steele <orie@transmute.industries>
2022-04-23 15:44:56 -0500

    :rocket:

--- src/createCompletion.test.js
+++ src/createCompletion.test.js
@@ -5,24 +5,15 @@ describe("createCompletion", () => {
     const response = await createCompletion({
       apiKey: process.env.OPENAI_API_KEY,
       engine: "text-davinci-002",
-      prompt: `
-A table summarizing the fruits from Goocrux:
-
-There are many fruits that were found on the recently discovered planet Goocrux. 
-There are neoskizzles that grow there, which are purple and taste like candy.
-There are also loheckles, which are a grayish blue fruit and are very tart, a little bit like a lemon. 
-Pounits are a bright green color and are more savory than sweet. 
-There are also plenty of loopnovas which are a neon pink flavor and taste like cotton candy. 
-Finally, there are fruits called glowls, 
-which have a very sour and bitter taste which is acidic and caustic, and a pale orange tinge to them.
-
-| Fruit | Color | Flavor |
-`,
-      temperature: 0,
-      max_tokens: 100,
-      top_p: 1.0,
-      frequency_penalty: 0.0,
-      presence_penalty: 0.0,
+      temperature: 0.3,
+      max_tokens: 120,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
+      prompt: `Write a recipe based on these ingredients and instructions:
+    Frito Pie
+    Ingredients: Fritos Chili Shredded cheddar cheese Sweet white or red onions, diced small Sour cream
+    Instructions:`,
     });
     // console.log(JSON.stringify(response, null, 2));
     expect(response.choices[0].finish_reason).toBe("stop");
