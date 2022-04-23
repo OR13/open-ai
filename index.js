7f0bdf885e9abf8c32686371853fd088afe14ae9
Orie Steele <orie@transmute.industries>
2022-04-23 16:03:34 -0500

    :rocket:

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ const getOpts = () => {
   return {
     apiKey: core.getInput("api-key"),
     engine: core.getInput("engine"),
-    // temperature: core.getInput("temperature"),
-    // max_tokens: core.getInput("max_tokens"),
-    // top_p: core.getInput("top_p"),
-    // frequency_penalty: core.getInput("frequency_penalty"),
-    // presence_penalty: core.getInput("presence_penalty"),
+    temperature: core.getInput("temperature"),
+    max_tokens: core.getInput("max_tokens"),
+    top_p: core.getInput("top_p"),
+    frequency_penalty: core.getInput("frequency_penalty"),
+    presence_penalty: core.getInput("presence_penalty"),
 
-    temperature: 0.3,
-    max_tokens: 120,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0,
     prompt: core.getInput("prompt"),
   };
 };
